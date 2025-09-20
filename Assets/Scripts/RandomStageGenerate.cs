3c5ba3d5f3ce13da3fe55f191b4046839731ce06
Hiraisun <koushi.2nd@gmail.com>
2025-09-21 08:47:58 +0900

    直通回避

--- Assets/Scripts/RandomStageGenerate.cs
+++ Assets/Scripts/RandomStageGenerate.cs
@@ -12,6 +12,8 @@ public class RandomStageGenerate : MonoBehaviour
     [SerializeField] private Vector2 asteroidSizeRange = new(1, 2);
     private List<GameObject> asteroids = new List<GameObject>();
 
+    [SerializeField] private Collider2D directLineCollider;
+
 
     // 難易度ごとのパラメータ辞書
     private static readonly Dictionary<int, (int asteroidCount, Vector2 asteroidSizeRange)> difficultyParams = new()
@@ -23,6 +25,11 @@ public class RandomStageGenerate : MonoBehaviour
         { 5, (40, new Vector2(0.5f, 2)) }
     };
 
+    public void Initialize()
+    {
+        
+    }
+
     public void GenerateStage(int difficulty)
     {
         ClearStage();
@@ -31,6 +38,11 @@ public class RandomStageGenerate : MonoBehaviour
         this.asteroidCount = asteroidCount;
         this.asteroidSizeRange = asteroidSizeRange;
         GenerateStage2();
+    }
+
+    private struct AsteroidData{
+        public Vector2 pos;
+        public float size;
     }
 
     // 案1: 完全ランダム
@@ -44,7 +56,6 @@ public class RandomStageGenerate : MonoBehaviour
             do{
                 pos = new Vector3(Random.Range(-stageSize.x, stageSize.x), Random.Range(-stageSize.y, stageSize.y), 0);
             } while (IsSafeArea(pos));
-
             asteroid.transform.position = pos;
 
             float size = Random.Range(asteroidSizeRange.x, asteroidSizeRange.y);
@@ -54,24 +65,59 @@ public class RandomStageGenerate : MonoBehaviour
     }
 
     // 案2: 位置ランダム、サイズはパーリンノイズ -> 採用
+    // 必ず直通ラインを遮るようにする
     public void GenerateStage2()
     {
-        for (int i = 0; i < asteroidCount; i++)
+        List<AsteroidData> asteroidDataList = new();
+
+        // 直通ラインを遮るように生成
+        bool occludedDirectLine = false;
+        while (true){
+            for (int i = 0; i < asteroidCount; i++)
+            {
+                AsteroidData asteroidData = new();
+                // 位置決定
+                do{
+                    asteroidData.pos = new Vector2(Random.Range(-stageSize.x, stageSize.x), Random.Range(-stageSize.y, stageSize.y));
+                } while (IsSafeArea(asteroidData.pos));
+                // サイズ決定
+                asteroidData.size = Mathf.Lerp(asteroidSizeRange.x, asteroidSizeRange.y, Mathf.PerlinNoise(asteroidData.pos.x, asteroidData.pos.y));
+
+                asteroidDataList.Add(asteroidData);
+
+                // 新たに直通ラインをさえぎったか?
+                if (!occludedDirectLine){
+                    RaycastHit2D hit = Physics2D.CircleCast(
+                        asteroidData.pos, 
+                        asteroidData.size*0.52f, 
+                        Vector2.zero, 0, 
+                        1 << directLineCollider.gameObject.layer
+                    );
+                    if (hit.collider != null){
+                        occludedDirectLine = true;  
+                    }
+                }
+            }
+
+            if (occludedDirectLine){
+                break;
+            }else{
+                Debug.Log("直通, 再生成");
+                asteroidDataList.Clear();
+            }
+        }
+
+        // 生成
+        foreach (AsteroidData asteroidData in asteroidDataList)
         {
             GameObject asteroid = Instantiate(asteroidPrefab);
-
-            Vector3 pos;
-            do{
-                pos = new Vector3(Random.Range(-stageSize.x, stageSize.x), Random.Range(-stageSize.y, stageSize.y), 0);
-            } while (IsSafeArea(pos));
-            asteroid.transform.position = pos;
-
-            float size = Mathf.Lerp(asteroidSizeRange.x, asteroidSizeRange.y, Mathf.PerlinNoise(pos.x, pos.y));
-            asteroid.transform.localScale = new Vector3(size, size, 1);
-
+            asteroid.transform.position = new Vector3(asteroidData.pos.x, asteroidData.pos.y, 0);
+            asteroid.transform.localScale = new Vector3(asteroidData.size, asteroidData.size, 1);
             asteroids.Add(asteroid);
         }
     }
+
+
 
     public void ClearStage()
     {
@@ -93,6 +139,15 @@ public class RandomStageGenerate : MonoBehaviour
             }
         }
         return false;
+    }
+
+    /// <summary>
+    /// 直接到達不可能かどうかをチェック
+    /// </summary>
+    private void CheckNotDirectlyReachable(){
+        // Instantiate はフレーム最後に呼ばれるので、
+        // 
+
     }
 
     private void OnDrawGizmosSelected()
